158c67ceb954d214a3c2bd7d6dd7d3dd8e16b9ee
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-10-29 16:25:56 +0000

    api updates

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://sharenow-y2ej.onrender.com';
 
 export const uploadFile = async (data) => {
     try {
