c17b859083c0f1434adb18edd5dd462b8d6158de
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-10-29 17:44:04 +0000

    Share Option ADDED!

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { useRef, useState, useEffect } from 'react';
-import { FiUpload, FiDownload } from 'react-icons/fi';
-import { uploadFile } from './services/api'; // Correct path to your uploadFile function
-import toast, { Toaster } from 'react-hot-toast'; // Assuming you've imported useToaster from react-hot-toast
+import { FiUpload, FiShare2, FiMail, FiFacebook, FiTwitter } from 'react-icons/fi';
+import { uploadFile } from './services/api';
+import toast, { Toaster } from 'react-hot-toast';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { EmailShareButton, FacebookShareButton, TwitterShareButton } from 'react-share';
 
 function App() {
   const [file, setFile] = useState('');
   const [result, setResult] = useState('');
+  const [copied, setCopied] = useState(false);
 
   const fileInputRef = useRef();
 
@@ -27,18 +30,20 @@ function App() {
       }
     };
     getImage();
-  }, [file, toast]);
+  }, [file]);
 
   const onUploadClick = () => {
     fileInputRef.current.click();
   };
 
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-200">
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-      />
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="bg-lime-200 p-8 flex flex-col items-center justify-center rounded-md shadow-md w-full sm:w-3/4 md:w-2/4 lg:w-1/3">
         <div className="flex justify-center items-center mb-6">
           <h1 className="text-2xl md:text-3xl font-bold">Simple File Sharing App!</h1>
@@ -63,16 +68,28 @@ function App() {
         </div>
 
         {result && (
-          <div className="text-center mb-6">
-          <button 
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex items-center justify-center"
-          onClick={() => window.open(result, '_blank')}
-          >
-            <FiDownload className="w-4 h-4 mr-2" />
-            <span>Download File</span>
-          </button>
-        </div>
-        
+          <div className="flex flex-col items-center justify-center text-center mb-6">
+            <CopyToClipboard text={result} onCopy={handleCopy}>
+              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex items-center">
+                <FiShare2 className="w-6 h-6 mr-2" />
+                <span>Copy Share Link</span>
+              </button>
+            </CopyToClipboard>
+
+            {copied && <div className="text-green-600">Link copied to clipboard!</div>}
+
+            <div className="flex items-center mt-4">
+              <EmailShareButton url={result}>
+                <FiMail className="text-blue-500 text-2xl cursor-pointer mr-2" />
+              </EmailShareButton>
+              <FacebookShareButton url={result}>
+                <FiFacebook className="text-blue-800 text-2xl cursor-pointer mr-2" />
+              </FacebookShareButton>
+              <TwitterShareButton url={result}>
+                <FiTwitter className="text-blue-400 text-2xl cursor-pointer" />
+              </TwitterShareButton>
+            </div>
+          </div>
         )}
       </div>
     </div>
