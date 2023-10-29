b5b792e626e2649d816b1acbfafd3497b3dd6ed8
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-10-29 18:19:05 +0000

    Security ADDED!

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,47 @@
 import { useRef, useState, useEffect } from 'react';
-import { FiUpload, FiShare2, FiMail, FiFacebook, FiTwitter } from 'react-icons/fi';
+import { FiShare2, FiMail, FiFacebook, FiTwitter } from 'react-icons/fi';
 import { uploadFile } from './services/api';
 import toast, { Toaster } from 'react-hot-toast';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { EmailShareButton, FacebookShareButton, TwitterShareButton } from 'react-share';
+import CryptoJS from 'crypto-js';
 
 function App() {
   const [file, setFile] = useState('');
   const [result, setResult] = useState('');
   const [copied, setCopied] = useState(false);
+  const [encrypting, setEncrypting] = useState(false);
+  const [decrypting, setDecrypting] = useState(false);
 
   const fileInputRef = useRef();
 
   useEffect(() => {
     const getImage = async () => {
       if (file) {
-        const data = new FormData();
-        data.append('name', file.name);
-        data.append('file', file);
+        setEncrypting(true); 
+        const reader = new FileReader();
 
-        toast.promise(uploadFile(data), {
-          loading: 'Uploading...',
-          success: (response) => {
-            setResult(response.path);
-            return <b>File uploaded successfully!</b>;
-          },
-          error: <b>Upload failed.</b>,
-        });
+        reader.onload = async function (event) {
+          const data = event.target.result;
+          const encrypted = CryptoJS.AES.encrypt(data, 'secret key').toString();
+
+          const encryptedFile = new File([encrypted], file.name, { type: file.type });
+          const formData = new FormData();
+          formData.append('name', encryptedFile.name);
+          formData.append('file', encryptedFile);
+
+          toast.promise(uploadFile(formData), {
+            loading: 'Uploading...',
+            success: (response) => {
+              setResult(response.path);
+              setEncrypting(false); 
+              return <b>File uploaded successfully!</b>;
+            },
+            error: <b>Upload failed.</b>,
+          });
+        };
+
+        reader.readAsDataURL(file);
       }
     };
     getImage();
@@ -41,12 +56,39 @@ function App() {
     setTimeout(() => setCopied(false), 3000);
   };
 
+  const handleDownload = async () => {
+    setDecrypting(true); // Set decryption status
+
+    // Logic for downloading and decrypting the file
+    if (result) {
+      const response = await fetch(result);
+      const data = await response.blob();
+
+      const reader = new FileReader();
+
+      reader.onload = function (event) {
+        const decrypted = CryptoJS.AES.decrypt(event.target.result, 'secret key').toString(CryptoJS.enc.Utf8);
+        const decryptedBlob = new Blob([decrypted], { type: data.type });
+        const downloadLink = window.URL.createObjectURL(decryptedBlob);
+
+        const a = document.createElement('a');
+        a.href = downloadLink;
+        a.download = file.name;
+        a.click();
+        window.URL.revokeObjectURL(downloadLink);
+        setDecrypting(false); // Reset decryption status
+      };
+
+      reader.readAsText(data);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-200">
       <Toaster position="top-center" reverseOrder={false} />
       <div className="bg-lime-200 p-8 flex flex-col items-center justify-center rounded-md shadow-md w-full sm:w-3/4 md:w-2/4 lg:w-1/3">
         <div className="flex justify-center items-center mb-6">
-          <h1 className="text-2xl md:text-3xl font-bold">Simple File Sharing App!</h1>
+          <h1 className="text-2xl md:text-3xl font-bold">File Sharing App!</h1>
         </div>
 
         <p className="text-center mb-6">Upload and share the download link.</p>
@@ -61,9 +103,9 @@ function App() {
           <button
             className="flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={onUploadClick}
+            disabled={encrypting} // Disable the button during encryption
           >
-            <FiUpload className="w-6 h-6 mr-2" />
-            Upload
+            {encrypting ? 'Encrypting...' : 'Upload'}
           </button>
         </div>
 
@@ -77,6 +119,14 @@ function App() {
             </CopyToClipboard>
 
             {copied && <div className="text-green-600">Link copied to clipboard!</div>}
+
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+              onClick={handleDownload}
+              disabled={decrypting} // Disable the button during decryption
+            >
+              {decrypting ? 'Decrypting...' : 'Download'}
+            </button>
 
             <div className="flex items-center mt-4">
               <EmailShareButton url={result}>
